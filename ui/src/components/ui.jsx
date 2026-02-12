1adfad369f39535d5b164c9aa7e39b8940052030
Stefan <stefankumarasinghe@gmail.com>
2026-02-12 20:23:11 +0530

    fixing ui warnings and console errors

--- ui/src/components/ui.jsx
+++ ui/src/components/ui.jsx
@@ -220,7 +220,7 @@ export function Input({ label, error, helperText, className, ...props }) {
 }
 
 Input.propTypes = {
-  label: PropTypes.string,
+  label: PropTypes.node,
   error: PropTypes.string,
   helperText: PropTypes.string,
   className: PropTypes.string,
@@ -261,7 +261,7 @@ export function Select({ label, error, helperText, children, className, ...props
 }
 
 Select.propTypes = {
-  label: PropTypes.string,
+  label: PropTypes.node,
   error: PropTypes.string,
   helperText: PropTypes.string,
   children: PropTypes.node.isRequired,
